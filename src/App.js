6547061fb27effc4c5af4ccbc5ee3ce2f6cc2c44
jatan-ops <jatan.shah90@gmail.com>
2022-09-07 19:07:54 +0530

    slate-js init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,160 @@
-import logo from './logo.svg';
-import './App.css';
+import { Slate, Editable, withReact } from 'slate-react'
+import { createEditor, Editor, Transforms, Text } from 'slate'
+import { useState, useCallback, useMemo } from 'react';
 
-function App() {
+const CustomEditor = {
+  isBoldMarkActive(editor) {
+    const [match] = Editor.nodes(editor, {
+      match: n => {
+        console.log('n isBoldMarkActive: ', n.children)
+        return n.bold === true
+      },
+      universal: true,  
+    })
+
+    console.log('bold match: ', [match])
+    return !!match
+  },
+
+  isCodeBlockActive(editor) {
+    const [match] = Editor.nodes(editor, {
+      match: n => n.type === 'code',
+    })
+
+    return !!match
+  },
+
+  toggleBoldMark(editor) {
+    const isActive = CustomEditor.isBoldMarkActive(editor)
+    Transforms.setNodes(
+      editor,
+      { bold: isActive ? null : true },
+      { match: n => Text.isText(n), split: true }
+    )
+  },
+
+  toggleCodeBlock(editor) {
+    const isActive = CustomEditor.isCodeBlockActive(editor)
+    Transforms.setNodes(
+      editor,
+      { type: isActive ? null : 'code' },
+      { match: n => Editor.isBlock(editor, n) }
+    )
+  },
+}
+
+const App = () => {
+
+  const [editor] = useState(() => withReact(createEditor()))
+
+  const initialValue = useMemo(
+    () =>
+      JSON.parse(localStorage.getItem('content')) || [
+        {
+          type: 'paragraph',
+          children: [{ text: 'A line of text in a paragraph.' }],
+        },
+      ],
+    []
+  )
+
+  const renderElement = useCallback(props => {
+    switch (props.element.type) {
+      case 'code':
+        return <CodeElement {...props} />
+      default:
+        return <DefaultElement {...props} />
+    }
+  }, [])
+
+  const renderLeaf = useCallback(props => {
+    return <Leaf {...props} />
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    // think of the <Slate> component as providing a context to every component underneath it.
+    <Slate 
+      editor={editor} 
+      value={initialValue}
+      onChange={value => {
+        const isAstChange = editor.operations.some(
+          op => 'set_selection' !== op.type
+        )
+        if (isAstChange) {
+          // Save the value to Local Storage.
+          const content = JSON.stringify(value)
+          localStorage.setItem('content', content)
+        }
+      }}
+    >
+       <div>
+        <button
+          onMouseDown={event => {
+            event.preventDefault()
+            CustomEditor.toggleBoldMark(editor)
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          Bold
+        </button>
+        <button
+          onMouseDown={event => {
+            event.preventDefault()
+            CustomEditor.toggleCodeBlock(editor)
+          }}
+        >
+          Code Block
+        </button>
+      </div>
+      <Editable
+        renderElement={renderElement}
+        renderLeaf={renderLeaf}
+        onKeyDown={event => {
+          if (!event.ctrlKey) {
+            return
+          }
+          
+          switch (event.key) {
+            // When "`" is pressed, keep our existing code block logic.
+            case '`': {              
+              event.preventDefault()
+              CustomEditor.toggleCodeBlock(editor)
+              break
+            }
+
+            // When "B" is pressed, bold the text in the selection.
+            case 'b': {
+              event.preventDefault()
+              CustomEditor.toggleBoldMark(editor)
+              break
+            }
+          }  
+        }}
+      />
+    </Slate>
+  )
+}
+
+const Leaf = props => {
+  return (
+    <span
+      {...props.attributes}
+      style={{ fontWeight: props.leaf.bold ? 'bold' : 'normal' }}
+    >
+      {props.children}
+    </span>
+  )
+}
+
+const CodeElement = props => {  
+  return (
+    <pre {...props.attributes}>
+      <code>{props.children}</code>
+    </pre>
+  )
+}
+
+const DefaultElement = props => {
+  return <p {...props.attributes}>{props.children}</p>
 }
 
 export default App;
