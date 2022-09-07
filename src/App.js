e083a8bc912e7bd746b5a0904cf90f5f569b1d76
jatan-ops <jatan.shah90@gmail.com>
2022-09-08 00:22:23 +0530

    simplified

--- src/App.js
+++ src/App.js
@@ -1,160 +1,53 @@
-import { Slate, Editable, withReact } from 'slate-react'
-import { createEditor, Editor, Transforms, Text } from 'slate'
 import { useState, useCallback, useMemo } from 'react';
 
-const CustomEditor = {
-  isBoldMarkActive(editor) {
-    const [match] = Editor.nodes(editor, {
-      match: n => {
-        console.log('n isBoldMarkActive: ', n.children)
-        return n.bold === true
-      },
-      universal: true,  
-    })
-
-    console.log('bold match: ', [match])
-    return !!match
-  },
-
-  isCodeBlockActive(editor) {
-    const [match] = Editor.nodes(editor, {
-      match: n => n.type === 'code',
-    })
-
-    return !!match
-  },
-
-  toggleBoldMark(editor) {
-    const isActive = CustomEditor.isBoldMarkActive(editor)
-    Transforms.setNodes(
-      editor,
-      { bold: isActive ? null : true },
-      { match: n => Text.isText(n), split: true }
-    )
-  },
-
-  toggleCodeBlock(editor) {
-    const isActive = CustomEditor.isCodeBlockActive(editor)
-    Transforms.setNodes(
-      editor,
-      { type: isActive ? null : 'code' },
-      { match: n => Editor.isBlock(editor, n) }
-    )
-  },
-}
+import RichEditor from './RichEditor';
 
 const App = () => {
 
-  const [editor] = useState(() => withReact(createEditor()))
-
-  const initialValue = useMemo(
-    () =>
-      JSON.parse(localStorage.getItem('content')) || [
-        {
-          type: 'paragraph',
-          children: [{ text: 'A line of text in a paragraph.' }],
-        },
-      ],
-    []
-  )
-
-  const renderElement = useCallback(props => {
-    switch (props.element.type) {
-      case 'code':
-        return <CodeElement {...props} />
-      default:
-        return <DefaultElement {...props} />
-    }
-  }, [])
-
-  const renderLeaf = useCallback(props => {
-    return <Leaf {...props} />
-  }, [])
+  const [input, setInput] = useState(initialValue);
 
   return (
-    // think of the <Slate> component as providing a context to every component underneath it.
-    <Slate 
-      editor={editor} 
-      value={initialValue}
-      onChange={value => {
-        const isAstChange = editor.operations.some(
-          op => 'set_selection' !== op.type
-        )
-        if (isAstChange) {
-          // Save the value to Local Storage.
-          const content = JSON.stringify(value)
-          localStorage.setItem('content', content)
-        }
-      }}
-    >
-       <div>
-        <button
-          onMouseDown={event => {
-            event.preventDefault()
-            CustomEditor.toggleBoldMark(editor)
-          }}
-        >
-          Bold
-        </button>
-        <button
-          onMouseDown={event => {
-            event.preventDefault()
-            CustomEditor.toggleCodeBlock(editor)
-          }}
-        >
-          Code Block
-        </button>
-      </div>
-      <Editable
-        renderElement={renderElement}
-        renderLeaf={renderLeaf}
-        onKeyDown={event => {
-          if (!event.ctrlKey) {
-            return
-          }
-          
-          switch (event.key) {
-            // When "`" is pressed, keep our existing code block logic.
-            case '`': {              
-              event.preventDefault()
-              CustomEditor.toggleCodeBlock(editor)
-              break
-            }
-
-            // When "B" is pressed, bold the text in the selection.
-            case 'b': {
-              event.preventDefault()
-              CustomEditor.toggleBoldMark(editor)
-              break
-            }
-          }  
-        }}
-      />
-    </Slate>
-  )
+    <>
+      <RichEditor value={input} setValue={setInput} />
+    </>
+  );
 }
 
-const Leaf = props => {
-  return (
-    <span
-      {...props.attributes}
-      style={{ fontWeight: props.leaf.bold ? 'bold' : 'normal' }}
-    >
-      {props.children}
-    </span>
-  )
-}
-
-const CodeElement = props => {  
-  return (
-    <pre {...props.attributes}>
-      <code>{props.children}</code>
-    </pre>
-  )
-}
-
-const DefaultElement = props => {
-  return <p {...props.attributes}>{props.children}</p>
-}
+const initialValue = [
+  {
+    type: 'paragraph',
+    children: [
+      { text: 'This is editable ' },
+      { text: 'rich', bold: true },
+      { text: ' text, ' },
+      { text: 'much', italic: true },
+      { text: ' better than a ' },
+      { text: '<textarea>', code: true },
+      { text: '!' }
+    ]
+  },
+  {
+    type: 'paragraph',
+    children: [
+      {
+        text:
+          "Since it's rich text, you can do things like turn a selection of text "
+      },
+      { text: 'bold', bold: true },
+      {
+        text:
+          ', or add a semantically rendered block quote in the middle of the page, like this:'
+      }
+    ]
+  },
+  {
+    type: 'block-quote',
+    children: [{ text: 'A wise quote.' }]
+  },
+  {
+    type: 'paragraph',
+    children: [{ text: 'Try it out for yourself!' }]
+  }
+];
 
 export default App;
